3e22c1cc45d140459d2c82f1f63e4beb4775699e
Rustam Satlykov <satlikov.rustam.mail@gmail.com>
2021-03-24 22:10:07 +0500

    feat: add case

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,25 +1,43 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(arr = []) {
-
-  if (Array.isArray(arr) === 'false') throw new Error('Is not array');
-  if (arr === []) throw new Error('Empty array');
+  if (Array.isArray(arr) == "false") throw new Error("Is not array");
 
   let newArr = [];
 
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] !== '--discard-prev' || arr[i] !== '--double-next' || arr[i] !== '--double-prev' || arr[i] !== '--discard-next') {
-      newArr.push(arr[i]);
-    } else if (arr[i] == '--discard-prev') {
-      newArr.pop();
-    } else if (arr[i] == '--double-next') {
-      newArr.push(arr[i + 1]);
-    } else if (arr[i] == '--double-prev') {
-        newArr.push(arr[i - 1]);
-    } else if (newArr[i] == '--discard-next') {
-      i++
-    } else {throw new Error('Not one of the options')}
+    switch (arr[i]) {
+      case "--discard-prev":
+        if (newArr.length !== 0) {
+          newArr.pop();
+        }
+        break;
+
+      case "--discard-next":
+        if (i < newArr.length) {
+          i++;
+        }
+        break;
+
+      case "--double-next":
+        if (i < newArr.length) {
+          newArr.push(arr[i + 1]);
+        }
+        break;
+
+      case "--double-prev":
+        if (arr[i - 2] == "--discard-next") {
+          newArr.pop();
+        } else if (newArr.length !== 0) {
+          newArr.push(arr[i - 1]);
+        }
+        break;
+
+      default:
+        newArr.push(arr[i]);
+        break;
+    }
   }
 
   return newArr;
-}
+};
