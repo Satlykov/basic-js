1dd7983b8b9aa43e209324bd29b59d898124585a
Rustam Satlykov <satlikov.rustam.mail@gmail.com>
2021-03-25 13:24:21 +0500

    feat: add repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,6 +1,9 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-    throw new CustomError('Not implemented');
+module.exports = function repeater(str, options) {
+    str = String(str);
+    addition = String(addition);
+    let additionFullStr =  `${addition}${(additionSeparator + addition).repeat(additionRepeatTimes - 1)}`;
+    let basicStr = `${str}${additionFullStr}`;
+    return resultStr = `${basicStr}${(separator + basicStr).repeat(repeatTimes - 1)}`
 };
-  
