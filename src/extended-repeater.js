833695a515ad1e2d8caddfa573099ce3e5b5e1b1
Rustam Satlykov <satlikov.rustam.mail@gmail.com>
2021-03-26 10:39:49 +0500

    fix: repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,11 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(str, options = {repeatTimes: 1, separator: '+', addition: '', additionRepeatTimes: 1, additionSeparator: '|'}) {
-    console.log(options.separator);
+module.exports = function repeater(str, options) {
     str = String(str);
     let addition = String(options.addition);
+    if (options.addition === undefined) {addition = ''};
     let additionSeparator = String(options.additionSeparator);
+    if (options.additionSeparator == undefined) {additionSeparator = '|'};
     let separator = options.separator;
+    if (options.separator == undefined) {separator = '+'};
     let additionFullStr =  `${addition}${(additionSeparator + addition).repeat(options.additionRepeatTimes - 1)}`;
     let basicStr = `${str}${additionFullStr}`;
     return resultStr = `${basicStr}${(separator + basicStr).repeat(options.repeatTimes - 1)}`;
