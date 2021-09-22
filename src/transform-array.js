2d2e05f6d4ab2c334cc89cbfd6fa009143c9bf8e
Rustam Satlykov <satlikov.rustam.mail@gmail.com>
2021-09-22 12:21:30 +0500

    feat: add solution vigenere-cipher

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,54 +1,55 @@
-import { NotImplementedError } from '../extensions/index.js';
+import { NotImplementedError } from "../extensions/index.js";
 
 /**
  * Create transformed array based on the control sequences that original
  * array contains
- * 
+ *
  * @param {Array} arr initial array
  * @returns {Array} transformed array
- * 
+ *
  * @example
- * 
+ *
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
+ *
  */
 export default function transform(arr) {
-  if (Array.isArray(arr) === false) throw new Error("\'arr\' parameter must be an instance of the Array!");
+    if (Array.isArray(arr) === false)
+        throw new Error("'arr' parameter must be an instance of the Array!");
 
-  let newArr = [];
+    let newArr = [];
 
-  for (let i = 0; i < arr.length; i++) {
-      switch (arr[i]) {
-          case "--discard-prev":
-              if (arr[i - 2] !== "--discard-next") {
-                  newArr.pop();
-              }
-              break;
+    for (let i = 0; i < arr.length; i++) {
+        switch (arr[i]) {
+            case "--discard-prev":
+                if (arr[i - 2] !== "--discard-next") {
+                    newArr.pop();
+                }
+                break;
 
-          case "--discard-next":
-              i++;
-              break;
+            case "--discard-next":
+                i++;
+                break;
 
-          case "--double-next":
-              if (arr[i + 1] !== undefined) {
-                  newArr.push(arr[i + 1]);
-              }
-              break;
+            case "--double-next":
+                if (arr[i + 1] !== undefined) {
+                    newArr.push(arr[i + 1]);
+                }
+                break;
 
-          case "--double-prev":
-              if (arr[i - 2] !== "--discard-next") {
-                  if (arr[i - 1] !== undefined) {
-                      newArr.push(arr[i - 1]);
-                  }
-              }
-              break;
+            case "--double-prev":
+                if (arr[i - 2] !== "--discard-next") {
+                    if (arr[i - 1] !== undefined) {
+                        newArr.push(arr[i - 1]);
+                    }
+                }
+                break;
 
-          default:
-              newArr.push(arr[i]);
-              break;
-      }
-  }
+            default:
+                newArr.push(arr[i]);
+                break;
+        }
+    }
 
-  return newArr;
+    return newArr;
 }
